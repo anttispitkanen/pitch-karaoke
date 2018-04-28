549c80238e62e281a5d67727ce5edc6359ceb74a
Antti <anttis.pitkanen@gmail.com>
2018-04-28 12:43:35 +0300

    make eslint happy

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,8 +5,8 @@ import './header.scss';
 
 const Header = () => (
     <header className="main-header">
-        <a href="http://tampere.es" target="_blank">
-            <img className="header-logo" src={logo} />
+        <a href="http://tampere.es" target="_blank" rel="noopener noreferrer">
+            <img className="header-logo" src={logo} alt="logo" />
         </a>
         <h1 className="main-heading">Pitch Karaoke</h1>
     </header>
