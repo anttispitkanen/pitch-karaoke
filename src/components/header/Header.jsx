789e86664003d3a60cc390d3e0c73a7c3d24f227
Antti <anttis.pitkanen@gmail.com>
2018-05-04 14:05:52 +0300

    style header, add FA

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from './logo.png';
 
 import './header.scss';
 
 const Header = () => (
     <header className="main-header">
+        <Link to="/"><i className="fa fa-home"></i></Link>
+        <h1 className="main-heading">Pitch Karaoke</h1>
         <a href="http://tampere.es" target="_blank" rel="noopener noreferrer">
             <img className="header-logo" src={logo} alt="logo" />
         </a>
-        <h1 className="main-heading">Pitch Karaoke</h1>
     </header>
 );
 
