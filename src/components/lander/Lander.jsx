fb1a80fa7fec57a82143a8f13d93de42983da119
Antti <anttis.pitkanen@gmail.com>
2018-05-06 12:54:58 +0300

    rename to Pitch KaraokES

--- src/components/lander/Lander.jsx
+++ src/components/lander/Lander.jsx
@@ -5,7 +5,7 @@ import './Lander.scss';
 
 const Lander = () => (
     <div className="lander">
-        <h2 className="lander-heading">Welcome to Pitch Karaoke!</h2>
+        <h2 className="lander-heading">Welcome to Pitch KaraokES!</h2>
         <p className="poison">Choose your poison:</p>
         <div className="links-container">
             <Link className="link quickfire" to="/quickfire">Quick Fire Pitching</Link>
