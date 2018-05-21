325915823730fdb52ad04dceef5ac8c219d12a40
Antti <anttis.pitkanen@gmail.com>
2018-05-21 22:47:48 +0300

    adjust for custom domain

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import RootRoutes from './RootRoutes';
 class App extends Component {
     render() {
         return (
-            <Router basename="/pitch-karaoke">
+            <Router>
                 <div className="App">
                     <Header />
                     <RootRoutes />
